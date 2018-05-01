1eed197129c92a1904b75949551ab1560b3a434f
boooooommmmmm <hushuyan88@gmail.com>
2018-05-01 23:55:15 +1000

    checkin

--- code/LSB_quickRUN.m
+++ code/LSB_quickRUN.m
@@ -3,17 +3,20 @@ imageNum = length(imageList);
 
 PSNR_List = zeros(imageNum, 1);
 PSNR_pair_List = zeros(imageNum, 1);
-PSNR_pair_new_List = zeros(imageNum, 1);
+PSNR_pair_dual_List = zeros(imageNum, 1);
+PSNR_pair_triple_List = zeros(imageNum, 1);
 PSNR_pair_ultra_List = zeros(imageNum, 1);
 
 Hae_List_LSB = zeros(imageNum, 1);
 Hae_List_LSB_pair = zeros(imageNum, 1);
-Hae_List_LSB_pair_new = zeros(imageNum, 1);
+Hae_List_LSB_pair_dual = zeros(imageNum, 1);
+Hae_List_LSB_pair_triple = zeros(imageNum, 1);
 Hae_List_LSB_pair_ultra = zeros(imageNum, 1);
 
 SSIM_List_LSB = zeros(imageNum, 1);
 SSIM_List_LSB_pair = zeros(imageNum, 1);
-SSIM_List_LSB_pair_new = zeros(imageNum, 1);
+SSIM_List_LSB_pair_dual = zeros(imageNum, 1);
+SSIM_List_LSB_pair_triple = zeros(imageNum, 1);
 SSIM_List_LSB_pair_ultra = zeros(imageNum, 1);
 
 Image_List = cell(imageNum, 1);
@@ -25,93 +28,94 @@ for i = 1: 10
     Image_List{i} = imageName;
     hostImage = strcat('grayscale photo\', hostImage);      %hostImage = path
     
-    [hieght, width] = LSB_embed(hostImage, 'watermark.txt', strcat('LSB photo\', strcat(imageName, '.png')));
-    LSB_extract(hieght, width, strcat('LSB photo\', strcat(imageName, '.png')), strcat('extract\',strcat(imageName, '.txt')));
+    [hieght, width] = LSB_embed(hostImage, 'watermark.txt', strcat('LSB_photo\', strcat(imageName, '.png')));
+    LSB_extract(hieght, width, strcat('LSB_photo\', strcat(imageName, '.png')), strcat('extract_LSB\',strcat(imageName, '.txt')));
     
-    [hieght, width] = LSB_pair_1(hostImage, 'watermark.txt', strcat('LSB-pair photo\', strcat(imageName, '.png')));
-    LSB_extract(hieght, width, strcat('LSB-pair photo\', strcat(imageName, '.png')), strcat('extract_pair\',strcat(imageName, '.txt')));
-    %
-    %     [hieght, width] = LSB_pair_1(hostImage, 'watermark.txt', strcat('LSB-pair1 photo\', strcat(imageName, '.png')));
-    %     LSB_extract(hieght, width, strcat('LSB-pair1 photo\', strcat(imageName, '.png')), strcat('extract_pair1\',strcat(imageName, '.txt')));
+    [hieght, width] = LSB_pair_1(hostImage, 'watermark.txt', strcat('LSB_pair_photo\', strcat(imageName, '.png')));
+    LSB_extract(hieght, width, strcat('LSB_pair_photo\', strcat(imageName, '.png')), strcat('extract_pair\',strcat(imageName, '.txt')));
     
-    %     [hieght, width] = LSB_pair_new(hostImage, 'watermark.txt', strcat('LSB_pair_new photo\', strcat(imageName, '.png')));
-    %     LSB_extract(hieght, width, strcat('LSB_pair_new photo\', strcat(imageName, '.png')), strcat('extract_LSB_pair_new\',strcat(imageName, '.txt')));
+    [hieght, width] = LSB_pair_dual(hostImage, 'watermark.txt', strcat('LSB_pair_dual_photo\', strcat(imageName, '.png')));
+    LSB_extract(hieght, width,strcat('LSB_pair_dual_photo\', strcat(imageName, '.png')), strcat('extract_LSB_pair_dual\',strcat(imageName, '.txt')));
     
-    [hieght, width] = LSB_pair_dual(hostImage, 'watermark.txt', strcat('LSB_pair_ultra photo\', strcat(imageName, '.png')));
-    LSB_extract(hieght, width,strcat('LSB_pair_ultra photo\', strcat(imageName, '.png')), strcat('extract_LSB_pair_ultra\',strcat(imageName, '.txt')));
+    [hieght, width] = LSB_pair_triple(hostImage, 'watermark.txt', strcat('LSB_pair_triple_photo\', strcat(imageName, '.png')));
+    LSB_extract(hieght, width,strcat('LSB_pair_triple_photo\', strcat(imageName, '.png')), strcat('extract_LSB_pair_triple\',strcat(imageName, '.txt')));
     
     
     hostImg = imread(hostImage);
-    watermarkedImg = imread(strcat('LSB photo\', strcat(imageName, '.png')));
-    watermarkedImg_pair = imread(strcat('LSB-pair photo\', strcat(imageName, '.png')));
-    watermarkedImg_pair_ultra = imread(strcat('LSB_pair_ultra photo\', strcat(imageName, '.png')));
-    %     watermarkedImg_pair1 = imread(strcat('LSB-pair1 photo\', strcat(imageName, '.png')));
-    
-    
+    watermarkedImg = imread(strcat('LSB_photo\', strcat(imageName, '.png')));
+    watermarkedImg_pair = imread(strcat('LSB_pair_photo\', strcat(imageName, '.png')));
+    watermarkedImg_pair_dual = imread(strcat('LSB_pair_photo\', strcat(imageName, '.png')));
+    watermarkedImg_pair_triple = imread(strcat('LSB_pair_photo\', strcat(imageName, '.png')));
+%     watermarkedImg_pair_ultra = imread(strcat('LSB_pair_ultra photo\', strcat(imageName, '.png')));        
+        
     % PSNR
     PSNR1 = PSNR(hostImg, watermarkedImg);
     PSNR_pair = PSNR(hostImg, watermarkedImg_pair);
-    PSNR_pair_ultra = PSNR(hostImg, watermarkedImg_pair_ultra);
-    %     PSNR_pair1 = PSNR(hostImg, watermarkedImg_pair1);
+    PSNR_pair_dual = PSNR(hostImg, watermarkedImg_pair_dual);
+    PSNR_pair_triple = PSNR(hostImg, watermarkedImg_pair_triple);
+%     PSNR_pair_ultra = PSNR(hostImg, watermarkedImg_pair_ultra);
+
     PSNR_List(i) = PSNR1;
     PSNR_pair_List(i) = PSNR_pair;
-    PSNR_pair_ultra_List(i) = PSNR_pair_ultra;
-    %     PSNR_pair1_List(i) = PSNR_pair1;
+    PSNR_pair_dual_List(i) = PSNR_pair_dual;
+    PSNR_pair_triple_List(i) = PSNR_pair_triple;
+%     PSNR_pair_ultra_List(i) = PSNR_pair_ultra;
+
     
     
     % Hae% Hm[n]
     Hmx = 0:255;
     Hm = Hae(hostImg, watermarkedImg);
     Hm_pair = Hae(hostImg, watermarkedImg_pair);
-    %     Hm_pair_new = Hae(hostImg, watermarkedImg_pair_new);
-    Hm_pair_ultra = Hae(hostImg, watermarkedImg_pair_ultra);
+    Hm_pair_dual = Hae(hostImg, watermarkedImg_pair_dual);
+    Hm_pair_triple = Hae(hostImg, watermarkedImg_pair_triple);
+%     Hm_pair_ultra = Hae(hostImg, watermarkedImg_pair_ultra);
     
     Hm1 = abs(Hm);
-    Hm_pair1 = abs(Hm_pair);
-    %     Hm_pair_new = abs(Hm_pair_new);
-    Hm_pair_ultra1 = abs(Hm_pair_ultra);
+    Hm_pair = abs(Hm_pair);
+    Hm_pair_dual = abs(Hm_pair_dual);
+    Hm_pair_triple = abs(Hm_pair_triple);
+%     Hm_pair_ultra = abs(Hm_pair_ultra);
     
     Hae_List_LSB(i) = sum(Hm1);
-    Hae_List_LSB_pair(i) = sum(Hm_pair1);
-    %     Hae_List_LSB_pair_new(i) = sum(Hm_pair_new);
-    Hae_List_LSB_pair_ultra(i) = sum(Hm_pair_ultra1);
+    Hae_List_LSB_pair(i) = sum(Hm_pair);
+    Hae_List_LSB_pair_dual(i) = sum(Hm_pair_dual);
+    Hae_List_LSB_pair_triple(i) = sum(Hm_pair_triple);
+%     Hae_List_LSB_pair_ultra(i) = sum(Hm_pair_ultra);
     
     %SSIM
     SSIM1 = SSIM(hostImg, watermarkedImg);
     SSIM_pair = SSIM(hostImg, watermarkedImg_pair);
-    SSIM_pair_ultra = SSIM(hostImg, watermarkedImg_pair_ultra);
+    SSIM_pair_dual = SSIM(hostImg, watermarkedImg_pair_dual);
+    SSIM_pair_triple = SSIM(hostImg, watermarkedImg_pair_triple);
+%     SSIM_pair_ultra = SSIM(hostImg, watermarkedImg_pair_ultra);
     
     SSIM_List_LSB(i) = SSIM1;
     SSIM_List_LSB_pair(i) = SSIM_pair;
-    %     SSIM_List_LSB_pair_new(i) = SSIM_pair_new;
-    SSIM_List_LSB_pair_ultra(i) = SSIM_pair_ultra;
+    SSIM_List_LSB_pair_dual(i) = SSIM_pair_dual;
+    SSIM_List_LSB_pair_triple(i) = SSIM_pair_triple;
+%     SSIM_List_LSB_pair_ultra(i) = SSIM_pair_ultra;
     
     fprintf('Processing... (%d of %d) has been done...\n', i,imageNum);
 end
 
 
-
-% PSNR_header = {'Image Name', 'LSB', 'LSB_pair', 'LSB_pair1', 'LSB-pair-ultar'};
-% PSNR1_List = [PSNR_List, PSNR_pair_List, PSNR_pair1_List, PSNR_pair_ultra_List];
-% xlswrite('PSNR.xlsx', PSNR_header);
-% xlswrite('PSNR.xlsx', Image_List, 'Sheet1', 'A2');
-% xlswrite('PSNR.xlsx', PSNR1_List, 'Sheet1', 'B2');
-
-PSNR_header = {'Image Name', 'LSB', 'LSB-pair' 'LSB-pair-ultar'};
-PSNR1_List = [PSNR_List, PSNR_pair_List, PSNR_pair_ultra_List];
+% save the process result to Excel
+PSNR_header = {'Image Name', 'LSB', 'LSB_pair' 'LSB_pair_dual', 'LSB_pair_triple'};
+PSNR1_List = [PSNR_List, PSNR_pair_List, PSNR_pair_dual_List, PSNR_pair_triple_List];
 xlswrite('PSNR.xlsx', PSNR_header);
 xlswrite('PSNR.xlsx', Image_List, 'Sheet1', 'A2');
 xlswrite('PSNR.xlsx', PSNR1_List, 'Sheet1', 'B2');
 
 
-Hae_header = {'Image Name', 'LSB', 'LSB_pair', 'LSB-pair-ultar'};
-Hae_List = [Hae_List_LSB, Hae_List_LSB_pair, Hae_List_LSB_pair_ultra];
+Hae_header = {'Image Name', 'LSB', 'LSB_pair' 'LSB_pair_dual', 'LSB_pair_triple'};
+Hae_List = [Hae_List_LSB, Hae_List_LSB_pair, Hae_List_LSB_pair_dual, Hae_List_LSB_pair_triple];
 xlswrite('Hae.xlsx', Hae_header);
 xlswrite('Hae.xlsx', Image_List, 'Sheet1', 'A2');
 xlswrite('Hae.xlsx', Hae_List, 'Sheet1', 'B2');
 
-SSIM_header = {'Image Name', 'LSB', 'LSB_pair', 'LSB-pair-ultar'};
-SSIM_List = [SSIM_List_LSB, SSIM_List_LSB_pair, SSIM_List_LSB_pair_ultra];
+SSIM_header = {'Image Name', 'LSB', 'LSB_pair' 'LSB_pair_dual', 'LSB_pair_triple'};
+SSIM_List = [SSIM_List_LSB, SSIM_List_LSB_pair, SSIM_List_LSB_pair_dual, SSIM_List_LSB_pair_triple];
 xlswrite('SSIM.xlsx', SSIM_header);
 xlswrite('SSIM.xlsx', Image_List, 'Sheet1', 'A2');
 xlswrite('SSIM.xlsx', SSIM_List, 'Sheet1', 'B2');
